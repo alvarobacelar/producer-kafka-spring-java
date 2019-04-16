110581f392f6d0403f8c598aaa56a7777472d380
Álvaro Bacelar <alvarobacelar@outlook.com>
2019-04-16 11:34:26 -0300

    Refatoraçao da mensagem e do ip do kafka

--- src/main/java/com/kafka/productor/ProductorApplication.java
+++ src/main/java/com/kafka/productor/ProductorApplication.java
@@ -12,45 +12,50 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductorApplication {	
-	
-	private static String kafka_server = System.getenv("KAFKA_SERVER");
+public class ProductorApplication {
+
+	private static String kafka_servers = System.getenv("KAFKA_SERVER");
 	private static String kafka_topic = System.getenv("KAFKA_TOPIC");
-	private static String kafka_count_msg = System.getenv("KAFKA_COUNT_MSG");	
+	private static String kafka_count_msg = System.getenv("KAFKA_COUNT_MSG");
+	private static String kafka_msg = System.getenv("KAFKA_MSG");
+
+	private static String mensagem = null;
 
 	private final static String TOPIC = kafka_topic;
-	private final static String BOOTSTRAP_SERVERS = kafka_server+":9092";
+	private final static String BOOTSTRAP_SERVERS = kafka_servers;
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ProductorApplication.class, args);
-		
+
 		if (args.length == 0) {
 			int num_msg_kafka = Integer.parseInt(kafka_count_msg);
-	        runProducer(num_msg_kafka);
-	    } else {
-	        runProducer(Integer.parseInt(args[0]));
-	    }
+			runProducer(num_msg_kafka);
+		} else {
+			runProducer(Integer.parseInt(args[0]));
+		}
 
 	}
-	
+
 	private static Producer<Long, String> createProducer() {
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                                            BOOTSTRAP_SERVERS);
-        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-                                        LongSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                                    StringSerializer.class.getName());
-        return new KafkaProducer<>(props);
-    }
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+		props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaProducer");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		return new KafkaProducer<>(props);
+	}
 
 	static void runProducer(final int sendMessageCount) throws Exception {
 		final Producer<Long, String> producer = createProducer();
 		long time = System.currentTimeMillis();
+		if (kafka_msg != null) {
+			mensagem = kafka_msg;
+		} else {
+			mensagem = "Mensagem enviada para o kafka com hash:";
+		}
 		try {
 			for (long index = time; index < time + sendMessageCount; index++) {
-				final ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, index, "Mensagem de hash - " + index);
+				final ProducerRecord<Long, String> record = new ProducerRecord<>(TOPIC, index, mensagem + " " + index);
 				RecordMetadata metadata = producer.send(record).get();
 				long elapsedTime = System.currentTimeMillis() - time;
 				System.out.printf("-------------- message productor ----------- \n");
@@ -59,7 +64,8 @@ public class ProductorApplication {
 				System.out.println("Record partition: " + metadata.partition());
 				System.out.println("Record Time: " + elapsedTime);
 				System.out.println("Record offset: " + metadata.offset());
-				System.out.println("Server Kafka: " + kafka_server);
+				System.out.println("Server Kafka: " + kafka_servers);
+				System.out.printf("-------------------------------------------- \n");
 			}
 		} finally {
 			producer.flush();
