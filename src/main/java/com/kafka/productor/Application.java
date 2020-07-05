3e1c6573ae5e492685f2dcec0c5b2d83053175d1
alvarobacelar <alvarobsttl@gmail.com>
2020-07-05 19:38:23 -0300

    Adicionado a versão web do spring

---
+++ src/main/java/com/kafka/productor/Application.java
@@ -0,0 +1,22 @@
+package com.kafka.productor;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/kafka/productor/controller/EventStreamsController.java
@@ -0,0 +1,32 @@
+package com.kafka.productor.controller;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+@RestController
+public class EventStreamsController {
+    @Autowired
+    private KafkaTemplate<String, String> template;
+    private List<String> messages = new CopyOnWriteArrayList<>();
+    @KafkaListener(topics = "${listener.topic}", groupId = "channel1")
+    public void listen(ConsumerRecord<String, String> record) throws Exception {
+        messages.add(record.value());
+    }
+    @GetMapping(value = "/send/{msg}")
+    public void send(String msg) throws Exception {
+        this.template.send("topic", msg);
+    }
+    @GetMapping("/messages")
+    public String received() throws Exception {
+        String result = messages.toString();
+        messages.clear();
+        return result;
+    }
+}
